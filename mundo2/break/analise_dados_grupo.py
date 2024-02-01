8bf4d9dfbcd0cd892e312e41d74f7791a4fe4c47
duduzada <eduardobarbosaalmeida@gmail.com>
2024-02-01 16:30:19 -0300

    novo exercicio

--- mundo2/break/analise_dados_grupo.py
+++ mundo2/break/analise_dados_grupo.py
@@ -4,25 +4,33 @@ A) quantas pessoas tem mais de 18 anos.
 B) quantos homens foram cadastrados.
 C) quantas mulheres tem menos de 20 anos'''
 
-op = ''
-sexo = ''
-cont = 0
+
+contA = 0
+contB = 0
+contC = 0
 while True:
     print("----------------------------------------")
     print("          CADASTRE UMA PESSOA ")
     print("----------------------------------------")
-    
+    op = ''
+    sexo = ''
+
     idade = int(input("Idade: "))
     while sexo not in ['M','F']:
         sexo = str(input("Sexo: [M/F] ")).upper().strip()[0]
+    if idade > 18:
+        contA += 1
+    if sexo == 'M':
+        contB += 1 
+    if idade < 20 and sexo == 'F':
+        contC += 1
     while op not in ['S','N']:
         op = str(input("Quer continuar? [S/N] ")).upper().strip()[0]
-        if op == 'N':
-            print()
-            break
-    if idade >= 18:
-        cont+=1
-print("acabou")
+    if op == 'N':
+        break
+print(f"Total de pessoas com mais de 18 anos: {contA}")
+print(f"Foram cadastrados {contB} homens.")
+print(f"Temos {contC}" "mulheres" if contC > 1 else "mulher" "cadastratadas com menos de 20 anos.")
     
 
     
