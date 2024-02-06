5e3b81c4f9e4b5db188d88964c5b8f81f03521fc
duduzada <eduardobarbosaalmeida@gmail.com>
2024-02-06 15:53:20 -0300

    tuplas

---
+++ mundo3/a.py
@@ -0,0 +1,35 @@
+#tuplas
+#lanche = ("Hamburger", "Suco" ,"Pizza", "Pudim")
+#print(lanche[0:2]) só ira exibir de 0 ate 1, 2 não será exibido
+#print(lanche[1:])  comece no 1 até o final
+#print(lanche[-1])  exibe o último valor
+#len(lanche) quantos valores há na variavel
+#print(lanche[:2]) do inicio até o elemento 2
+#for p in lanche:
+    #print(p) #irá repitir todas váriaveis
+
+#print(len(lanche))
+#for p in lanche:
+   # print(f"Eu vou comer {p}")
+
+#for cont in range(0, len(lanche)):
+   # print(f"Eu vou comer {lanche[cont]} na posição {cont}")
+
+#for pos ,p in enumerate(lanche):
+  # print(f"Eu vou comer {p} na posição {pos}")
+
+#print(sorted(lanche))
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b
+#print(c)
+#print(len(c))
+#print(c.count(5))
+#print(c.index(5))
+#print(c.index(5, 2)) # quero procurar a partir da posição
+
+
+#pessoa = ("Eduardo", 21, "M", 70)
+#del(pessoa)
+#print(pessoa)
