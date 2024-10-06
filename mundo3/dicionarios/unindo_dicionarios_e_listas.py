7613e4523b0741b7978eddc7a0d3d27b134ce37e
duduzada <eduardobarbosaalmeida@gmail.com>
2024-10-05 22:22:57 -0300

    alterações

--- mundo3/dicionarios/unindo_dicionarios_e_listas.py
+++ mundo3/dicionarios/unindo_dicionarios_e_listas.py
@@ -6,6 +6,7 @@ D) Uma lista de pessoas com idade acima da média'''
 
 todos = []
 pessoa = {}
+soma = media = 0
 while True:
     pessoa.clear()
     pessoa ['nome'] = str(input("Nome: "))
@@ -15,7 +16,8 @@ while True:
             break
         print("ERRO! Por favor, digite apenas M ou F.")
     pessoa['idade'] = int(input("Idade: "))
-    galera.append(pessoa.copy())
+    soma += pessoa['idade']
+    todos.append(pessoa.copy())
     while True:
         resp = str(input("Quer continuar? [S/N] ")).upper()[0]
         if resp in 'SN':
@@ -23,8 +25,25 @@ while True:
         print("ERRO! Responda apenas S ou N. ")
     if resp == 'N':
         break
-
 print("-=" *30)
+print(f"A) Ao todo temos {len(todos)} pessoas cadastradas.")
+media = soma / len(todos)
+print(f"B) A média de idade é de {media:5.2f} anos.")
+print("C) As mulheres cadastradas foram ", end="")
+for p in todos:
+    if p["sexo"] in "Ff":
+        print(f"{p['nome']} ", end="")
+
+print("D) Lista das pessoas que estão acima da média: ", end="")
+for p in todos:
+    if p["idade"] >= media:
+        print("       ", end="")
+        for k, v in p.items():
+            print(f"{k} = {v}: ", end="")
+        print()
+print(" <<ENCERRADO >>")
+
+
 
 
 
